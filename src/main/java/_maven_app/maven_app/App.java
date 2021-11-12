bfb95ff923721deac8dd13dc6b06e615f0a8729d
hr095048 <hr095048@gmail.com>
2021-11-12 08:59:24 +0530

    new project structure

---
+++ src/main/java/_maven_app/maven_app/App.java
@@ -0,0 +1,13 @@
+package _maven_app.maven_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
